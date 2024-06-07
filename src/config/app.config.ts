e22bba6fb6070487a458c06b5bf4fee645060e43
abhishek@millicent.in <abhishek@millicent.in>
2024-06-07 14:50:45 +0530

    updated config

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -7,7 +7,7 @@ export class AppConfig {
 
   constructor() {
     const environment = process.env.NODE_ENV || 'dev';
-    const envFilePath = path.resolve(__dirname, `../../dev.env`);
+    const envFilePath = path.resolve(__dirname, `../../.env`);
     this.envConfig = dotenv.parse(fs.readFileSync(envFilePath));
   }
 

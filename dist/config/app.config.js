7d25cad665227151ce1471f918b82583d8c16b60
abhishek@millicent.in <abhishek@millicent.in>
2024-06-07 15:18:24 +0530

    updated config

--- dist/config/app.config.js
+++ dist/config/app.config.js
@@ -7,8 +7,7 @@ const path = require("path");
 class AppConfig {
     constructor() {
         const environment = process.env.NODE_ENV || 'dev';
-        const envFilePath = path.resolve(__dirname, `../../.env`);
-        console.log('envFilePath:', envFilePath);
+        const envFilePath = path.resolve(__dirname, `../../${environment}.env`);
         this.envConfig = dotenv.parse(fs.readFileSync(envFilePath));
     }
     get(key) {

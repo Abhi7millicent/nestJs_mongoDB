c72c3b507dba88e0052481964a983e6f53ddd710
abhishek@millicent.in <abhishek@millicent.in>
2024-06-07 13:03:07 +0530

    updated module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { AppConfig } from 'src/config/app.config';
 import { ConfigModule } from 'src/config/config.module';
 import { DatabaseConfig } from 'src/database/database.config';
 import { Modules } from './modules/modules';
-import { ProcessDocumentModule } from './modules/process/features/process-document/process-document.module';
-import { QueriesResponsesModule } from './modules/process/features/queries-responses/queries-responses.module';
 
 @Module({
   imports: [
     ConfigModule,
     MongooseModule.forRootAsync({
-      imports: [ConfigModule, ProcessDocumentModule, QueriesResponsesModule],
+      imports: [ConfigModule],
       useClass: DatabaseConfig,
       inject: [AppConfig],
     }),
@@ -20,4 +18,4 @@ import { QueriesResponsesModule } from './modules/process/features/queries-respo
   // controllers: [],
   providers: [DatabaseConfig],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/modules/process/process.module.ts
+++ src/app/modules/process/process.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { ActivitiesModule } from './features/activities/activities.module';
 import { BasicDataModule } from './features/basic-data/basic-data.module';
 import { ControllerMonitoringModule } from './features/controller-and-monitoring/controller-and-monitoring.module';
+import { ProcessDocumentModule } from './features/process-document/process-document.module';
+import { QueriesResponsesModule } from './features/queries-responses/queries-responses.module';
 
 @Module({
-  imports: [ActivitiesModule, BasicDataModule, ControllerMonitoringModule],
+  imports: [
+    ActivitiesModule,
+    BasicDataModule,
+    ControllerMonitoringModule,
+    ProcessDocumentModule,
+    QueriesResponsesModule,
+  ],
 })
 export class ProcessModule {}

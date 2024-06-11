8728002ed2abe069d8519f52d64c92fe4ef4d011
abhishek@millicent.in <abhishek@millicent.in>
2024-06-11 10:58:48 +0530

    collection change

--- src/app/modules/process/process.schema.ts
+++ src/app/modules/process/process.schema.ts
@@ -508,7 +508,7 @@ class ProcessControl {
   is_deleted!: boolean;
 }
 
-@Schema({ collection: 'process_basic_data2' })
+@Schema({ collection: 'process_basic_data3' })
 export class Process extends Document {
   @Prop({ type: MongooseSchema.Types.ObjectId, auto: true })
   _id: MongooseSchema.Types.ObjectId;
